9e9ae7257c5274fc11abef26978c9ea86c6c2526
noob560 <74797066+noob560@users.noreply.github.com>
2020-11-20 16:55:50 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,38 @@
+for (let index = 0; index < 4; index++) {
+    console.log("1")
+    scene.setBackgroundColor(8)
+    console.log("2")
+    pause(100)
+    console.log("3")
+    scene.setBackgroundColor(15)
+    console.log("4")
+    pause(100)
+    console.log("505")
+    scene.setBackgroundColor(8)
+    console.log("6")
+}
+console.log("7")
+scene.setBackgroundColor(2)
+console.log("8")
+let mySprite = sprites.create(img`
+    8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+    8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+    8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+    8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+    8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+    8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+    8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+    8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+    8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+    8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+    8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+    8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+    8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+    8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+    8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+    8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 8 
+    `, SpriteKind.Player)
+controller.moveSprite(mySprite)
+forever(function () {
+    mySprite.setFlag(SpriteFlag.StayInScreen, true)
+})
